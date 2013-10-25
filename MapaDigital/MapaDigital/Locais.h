8087c531d1493857e5e4e7427bc437c8b70edd77
1070344 <1070344@isep.ipp.pt>
2013-10-25 17:28:34 +0100

    transformação dos minutos em horas mais minutos

--- MapaDigital/MapaDigital/Locais.h
+++ MapaDigital/MapaDigital/Locais.h
@@ -34,7 +34,96 @@ class Locais
 		bool operator >(const Locais &loc);
 		bool operator <(const Locais &loc);
 		bool operator ==(const Locais &loc);*/
+		virtual void mostrarHora(int t,string tipo) const;
 		virtual void escrever (ostream & out) const;
 		virtual void escrever2 (ostream & out) const;
 };
+
+
+Locais::Locais()
+{
+	desc1 = "vazio";
+	desc2 = "vazio";
+}
+
+Locais::Locais(string d1)
+{
+	desc1 = d1;
+}
+
+
+Locais::Locais(string d1,string d2)
+{
+	desc1 = d1;
+	desc2 = d2;
+}
+
+
+Locais::Locais(const Locais &loc)
+{
+	setDescricao1(loc.desc1);
+	setDescricao2(loc.desc2);
+}
+
+
+Locais::~Locais()
+{
+}
+
+Locais * Locais::clone() const
+{
+	return new Locais (*this);
+}
+
+void Locais::setDescricao1(const string d1)
+{
+	desc1 = d1;
+}
+
+
+void Locais::setDescricao2(const string d2)
+{
+	desc2 = d2;
+}
+
+
+string Locais::getDescricao1()const
+{
+	return desc1;
+}
+
+string Locais::getDescricao2()const
+{
+	return desc2;
+}
+
+void Locais::escrever(ostream & out) const
+{  
+	cout << "DESCRICAO: " << desc1 << endl;
+}
+
+void Locais::escrever2(ostream & out) const
+{  
+	cout << "ORIGEM: " << desc1 << endl;
+	cout << "DESTINO: " << desc2 << endl;
+}
+
+void Locais::mostrarHora(int t,string tipo)const
+{
+	if(tipo == "abertura")
+	{
+		int horas = t / 60;
+		int minutos = t % 60;
+		cout << "HORARIO DE ABERTURA: " << horas << ":" << minutos << "h" << endl;
+	}
+	else
+	{
+		int horas = t / 60;
+		int minutos = t % 60;
+		cout << "HORARIO DE ENCERRAMENTO: " << horas << ":" << minutos << "h" << endl << endl;
+	}
+}
+
+
+
 #endif
